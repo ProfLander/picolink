e381467b6a78ed8b9e0bc9ebe9a6dcee10ce1395
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-09-22 21:31:26 +0100

    Add local emacs config

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((eval . (load (expand-file-name "develop/emacs.el"
+                                         (locate-dominating-file default-directory
+                                                                 ".dir-locals.el")))))))
---
+++ develop/emacs.el
@@ -0,0 +1,16 @@
+;; -*- lexical-binding: t; -*-
+
+(evil-define-key 'normal racket-repl-mode-map
+  (kbd "<up>") #'racket-repl-next-input)
+
+(evil-define-key 'normal racket-repl-mode-map
+  (kbd "<down>") #'racket-repl-next-input)
+
+(evil-define-key 'normal racket-repl-mode-map
+  (kbd "<left>") #'racket-repl-previous-prompt-or-run)
+
+(evil-define-key 'normal racket-repl-mode-map
+  (kbd "<right>") #'racket-repl-next-prompt-or-run)
+
+(evil-define-key 'normal racket-repl-mode-map
+  (kbd "RET") #'racket-repl-submit)
