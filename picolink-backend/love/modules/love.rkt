fc7e0f09eb47bbfa1e319248662657a3144cafc1
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-09-25 06:08:15 +0100

    First working love backend

---
+++ picolink-backend/love/modules/love.rkt
@@ -0,0 +1,60 @@
+#lang picolink/intrinsic
+
+; Functions
+getVersion
+hasDeprecationOutput
+isVersionComplatible
+setDeprecationOutput
+
+; Types
+Data
+Object
+Variant
+
+; Callbacks
+;; General
+draw
+errorhandler
+load
+lowmemory
+quit
+run
+threaderror
+update
+
+;;  Window
+directorydropped
+displayrotated
+filedropped
+focus
+mousefocus
+resize
+visible
+
+;; Keyboard
+keypressed
+keyreleased
+textedited
+textinput
+
+;; Mouse
+mousemoved
+mousepressed
+mousereleased
+wheelmoved
+
+;; Joystick
+gamepadaxis
+gamepadpressed
+gamepadreleased
+joystickadded
+joystickaxis
+joystickhat
+joystickpressed
+joystickreleased
+joystickremoved
+
+;; Touch
+touchmoved
+touchpressed
+touchreleased
