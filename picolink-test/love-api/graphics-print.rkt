23d1adaabbf19f346e40a9c9382501cecce2b126
Prof. Lander <1253239+ProfLander@users.noreply.github.com>
2026-09-26 06:09:22 +0100

    Implement simple DSL macros, set! for lambda

--- picolink-test/love-api/graphics-print.rkt
+++ picolink-test/love-api/graphics-print.rkt
@@ -2,19 +2,19 @@
 
 (require (in lua print)
          (in love/graphics [print graphics-print])
-         (in love/ext
-             set-love-draw
-             set-love-update
-             set-love-key-pressed))
+         (in love
+             draw
+             update
+             [keypressed key-pressed]))
 
-(set-love-draw
- (λ ()
-   (graphics-print "love draw" 400 300)))
+(set! draw
+      (λ ()
+        (graphics-print "love draw" 400 300)))
 
-(set-love-update
- (λ (dt)
-   (print "love update" dt)))
+(set! update
+      (λ (dt)
+        (print "love update" dt)))
 
-(set-love-key-pressed
- (λ (key)
-   (print "key pressed" key)))
+(set! key-pressed
+      (λ (key)
+        (print "key pressed" key)))
